deec974400d0702bd2ae39c509424cd8950bf5e5
theo ferguson <fergy546@yahoo.com>
2023-03-06 16:08:31 +0000

    complete

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,12 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+        int[] solution = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            solution[i] = Math.abs(nums[i]);
+        }
+
+        return solution;
     }
 }
